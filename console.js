322a38f37b725040e913e0aa67320de8f4b1576f
Mike Green <mike.is.green@gmail.com>
2015-07-15 09:59:23 -0400

    finished console bootstrap script

--- console.js
+++ console.js
@@ -3,6 +3,16 @@ var http       = require('q-io/http');
 var fs         = require('q-io/fs');
 var CloudFlare = require('./lib/cloudflare');
 
-var auth;
+var auth, cf;
 
-fs.read('auth.json').invoke('toString').then(JSON.parse).then(function(json) { auth = json; });
+fs.exists('auth.json').then(function(exists) {
+  if (exists) {
+    return fs.read('auth.json');
+  }
+  throw new Error('auth.json not found');
+}).fail(function(err) {
+  console.log(err);
+}).invoke('toString').then(JSON.parse).then(function(json) {
+  auth = json;
+  cf   = new CloudFlare(auth);
+});
